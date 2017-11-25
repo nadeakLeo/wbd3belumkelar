565ea234b5daa489e1e5ffa8373c7864eee56dff
Muhammad Treza Nolandra <rezardes@gmail.com>
2017-11-25 15:08:43 +0700

    add basic http post request angularjs

--- ChatService/routes/send.js
+++ ChatService/routes/send.js
@@ -4,6 +4,18 @@ var router = express.Router();
 /* POST CHAT */
 router.post('/', function(req, res, next) {
 
+    console.log('POST REQUEST send');
+
+    // MENAMBAHKAN DATA CHAT KE DATABASE MONGODB
+
+    // MENGIRIM CHAT KE FIREBASE
+
+});
+
+router.get('/', function(req, res, next) {
+
+    console.log('GET REQUEST send');
+
     // MENAMBAHKAN DATA CHAT KE DATABASE MONGODB
 
     // MENGIRIM CHAT KE FIREBASE
